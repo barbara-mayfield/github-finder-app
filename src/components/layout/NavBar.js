f02bf6e424161c1b957830cc238351085f8b54b3
Barbara Mayfield <bmayfield1991@gmail.com>
2019-11-06 16:14:58 -0600

    Search bar and about section created

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,27 +1,34 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types';
 
-export class NavBar extends Component {
-    static defaultProps = {
-        title: 'Github Finder',
-        icon: 'fab fa-github'
-    };
-
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        icon: PropTypes.string.isRequired
-    }
+ const NavBar = ({ icon, title }) => {    
+    return (
+        <nav className='navbar bg-primary'>   
+            <h1>
+                <i className={icon} /> {title}
+            </h1>
+            <ul>
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/about'>About</Link>
+                </li>
+            </ul>
+        </nav>
+    )
     
-    render() {
-        return (
-            <div>
-                
-                <h1>
-                    <i className={this.props.icon} /> {this.props.title}
-                </h1>
-            </div>
-        )
-    }
+}
+
+NavBar.defaultProps = {
+    title: 'Github Finder',
+    icon: 'fab fa-github'
+};
+
+NavBar.propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
 }
 
 export default NavBar
