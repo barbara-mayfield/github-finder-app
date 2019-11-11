65a627dcdde881d98be5f0b7e460de36de6c6917
Barbara Mayfield <bmayfield1991@gmail.com>
2019-11-10 23:18:31 -0600

    added home, notfound pages, updated README

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Navbar from './components/layout/NavBar'
-import Search from './components/users/Search'
-import Users from './components/users/Users'
+import Home from './components/pages/Home'
+import About from './components/pages/About'
+import NotFound from './components/pages/NotFound'
+
 import User from './components/users/User'
 import Alert from './components/layout/Alert'
-import About from './components/pages/About'
 
 import GithubState from './context/github/GithubState'
 import AlertState from './context/alert/AlertState'
@@ -24,16 +25,10 @@ const App = () => {
               <div className="container">
                 <Alert />
                 <Switch>
-                  <Route exact path='/' render={props => (
-                    <Fragment>
-                      <Search/>
-                      <Users />
-                    </Fragment>
-                  )} />
-                  
+                  <Route exact path='/' component={Home} />
                   <Route exact path='/about' component={About} />
-                  
                   <Route exact path='/user/:login' component={User} />
+                  <Route component={NotFound} />
                 </Switch>
               </div>
           </div>
